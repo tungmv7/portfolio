5c40851cf7bd44f97a7fd4f69aa279bbb1125020
Tung Mang Vien <tungmv7@gmail.com>
2016-02-26 11:41:17 +0700

    update css

--- wp-content/themes/nordic/template-contact.php
+++ wp-content/themes/nordic/template-contact.php
@@ -37,42 +37,42 @@ get_header();
 	</div>
 
 
-	<div class="row contact-info">
-		<div class="contact-from col-sm-8">
-			<div class="row">
-				<div class="col-sm-12"><h5 class="section-title"><?php _e("Leave a message","um_lang"); ?></h5></div>
-			</div>
-			<div class="row">
-				<form action="<?php the_permalink(); ?>" class="col-sm-12" id="contact-page-form">
-					<div class="row">
-						<p class="col-sm-6"><input type="text" name="name" id="name" placeholder="<?php _e("Name","um_lang"); ?>"></p>
-						<p class="col-sm-6"><input type="email" name="email" id="email" placeholder="<?php _e("Email","um_lang"); ?>"></p>
-						<p class="col-sm-12"><textarea name="message" id="message" placeholder="<?php _e("Message","um_lang"); ?>"></textarea></p>
-						<p class="col-sm-12"><input type="submit" name="send" id="send" value="<?php _e("Send","um_lang"); ?>"></p>
-					</div>
-				</form>
-				<div class="col-sm-12">
-	                <div class="success-message alert alert-success">
-	                    <p>
-	                        <?php the_field("success_message"); ?>
-	                    </p>
-	                </div>
-                </div>
-			</div>
-		</div>
-        <?php if(get_field("address")): ?>
-		<div class="address col-sm-4">
-			<div class="row">
-				<div class="col-sm-12"><h5 class="section-title"><?php _e("About me","um_lang"); ?></h5></div>
-			</div>
-			<div class="row">
-				<div class="col-sm-12">
-					<p><?php the_field("address"); ?></p>
-				</div>
-			</div>
-		</div>
-        <?php endif; ?>
-	</div>
+<!--	<div class="row contact-info">-->
+<!--		<div class="contact-from col-sm-8">-->
+<!--			<div class="row">-->
+<!--				<div class="col-sm-12"><h5 class="section-title">--><?php //_e("Leave a message","um_lang"); ?><!--</h5></div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<form action="--><?php //the_permalink(); ?><!--" class="col-sm-12" id="contact-page-form">-->
+<!--					<div class="row">-->
+<!--						<p class="col-sm-6"><input type="text" name="name" id="name" placeholder="--><?php //_e("Name","um_lang"); ?><!--"></p>-->
+<!--						<p class="col-sm-6"><input type="email" name="email" id="email" placeholder="--><?php //_e("Email","um_lang"); ?><!--"></p>-->
+<!--						<p class="col-sm-12"><textarea name="message" id="message" placeholder="--><?php //_e("Message","um_lang"); ?><!--"></textarea></p>-->
+<!--						<p class="col-sm-12"><input type="submit" name="send" id="send" value="--><?php //_e("Send","um_lang"); ?><!--"></p>-->
+<!--					</div>-->
+<!--				</form>-->
+<!--				<div class="col-sm-12">-->
+<!--	                <div class="success-message alert alert-success">-->
+<!--	                    <p>-->
+<!--	                        --><?php //the_field("success_message"); ?>
+<!--	                    </p>-->
+<!--	                </div>-->
+<!--                </div>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--        --><?php //if(get_field("address")): ?>
+<!--		<div class="address col-sm-4">-->
+<!--			<div class="row">-->
+<!--				<div class="col-sm-12"><h5 class="section-title">--><?php //_e("About me","um_lang"); ?><!--</h5></div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-sm-12">-->
+<!--					<p>--><?php //the_field("address"); ?><!--</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--        --><?php //endif; ?>
+<!--	</div>-->
 </div>
 
-<?php //get_footer(); ?>
+<?php get_footer(); ?>
