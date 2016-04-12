03e6b72cd7e520989343785beacc8ee746754a15
Tung Mang Vien <tungmv7@gmail.com>
2016-04-12 22:39:19 +0700

    add themes

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/sources/profile/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'profile');
 
 /** MySQL database username */
